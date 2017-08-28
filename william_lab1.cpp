8e51dc548fd673b7ce62985c57a6998201d71be4
William Bell <williambell@William.local>
2017-08-28 13:07:57 -0400

    Final sort

--- william_lab1.cpp
+++ william_lab1.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+void Bubblesort(vector<string> &V);
 
 int main() {
+  int num_names;
+  string name;
+  vector<string> names;
+  cout << "How many names would you like to sort?: ";
+  cin >> num_names;
+  for(int i = 0; i < num_names; i++){
+    cout << "Enter the name: ";
+    cin >> name;
+    names.push_back(name);
+  }
+  cout << "**** Print Unsorted List ****" << endl;
+  for(int i = 0; i < num_names; i++){
+    cout << names.at(i) << "  ";
+  }
+  cout << endl;
+  Bubblesort(names);
+  cout << "**** Print Sorted List ****" << endl;
+  for(int i = 0; i < num_names; i++){
+    cout << names.at(i) << "  ";
+  }
+  cout << endl;
   return 0;
+}
+
+void Bubblesort(vector<string> &V){
+  string temp;
+  for(int i = 0; i < V.size(); i++){
+    for(int j = V.size() - 1; j > i; j--){
+      if(V.at(j-1).compare(V.at(j)) > 1){
+        temp = V.at(j);
+        V.at(j) = V.at(j-1);
+        V.at(j-1) = temp;
+      }
+    }
+  }
 }
